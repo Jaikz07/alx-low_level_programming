87d408f5a0938cc6c8bfe0e567da309d1b1a27de
jacob <jaysonjay695@gmail.com>
2023-02-16 08:06:35 -0800

    programme code1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - A program that prints a line with puts function 
+ * return; 0 (Sucessfully runned)
+ */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
