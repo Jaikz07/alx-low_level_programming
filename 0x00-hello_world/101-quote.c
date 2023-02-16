72c33b6a7c2187520f779adccac80b5faff97401
jacob <jaysonjay695@gmail.com>
2023-02-16 10:44:11 -0800

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,8 @@
  * main - A C program that prints a line to the standard error
  * Return: 1 (Success)
  */
-int main()
+int main(void)
 {
-    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t bytes_written = write(2, message, 51);
-    if (bytes_written != 51) {
-        return 1;
-    }
-    return 1;
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout;
+return (1);
 }
