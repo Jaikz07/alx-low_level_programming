4c4fdc82e43ba22204c7f7914fa7fff8691eddf3
jacob <jaysonjay695@gmail.com>
2023-02-17 15:39:30 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This program prints numbers without variable use.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+for (int j = 0; j < 10; j++)
+{
+	putchar('0' + j);
+}
+putchar('\n');
+return (0);
+}
