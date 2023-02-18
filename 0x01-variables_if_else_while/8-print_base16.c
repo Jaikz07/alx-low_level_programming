f4cfbb4f8e95cdd8dd0edaeee15f63d47cd3dd2a
jacob <jaysonjay695@gmail.com>
2023-02-17 16:24:29 -0800

    bases

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - this program prints in  base 16.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 16; i++)
+{
+if (i < 10)
+{
+	putchar('0' + i);
+}
+else
+{
+	putchar('a' + (i - 10));
+}
+}
+putchar('\n');
+return (0);
+}
+
