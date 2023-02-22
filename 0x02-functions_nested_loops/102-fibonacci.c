e0df5e2d97b7ba199939e033c657d6dad018057b
jacob <jaysonjay695@gmail.com>
2023-02-22 08:54:01 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - prints fibonacci sequence of numbers.
+ *Return: 0.
+ */
+int main(void)
+{
+int i, n = 50, t1 = 1, t2 = 2, j;
+printf("%d, %d, ", t1, t2);
+for (i = 3; i <= n; i++)
+{
+	j = t1 + t2;
+	t1 = t2;
+	t2 = j;
+	if (i != n)
+	{
+		printf("%d, ", j);
+	}
+	else
+	{
+		printf("%d\n", j);
+	}
+}
+return (0);
+}
