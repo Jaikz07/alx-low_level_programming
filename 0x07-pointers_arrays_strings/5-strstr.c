cafd22ddf0d097996ece066037ebe51051df1fca
jacob <jaysonjay695@gmail.com>
2023-03-06 10:05:28 -0800

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strstr - function finds the first occurrence of the substring needle 
+ * in the string haystack
+ * @haystack: string
+ * @needle: substring
+ * Return: pointer of substring or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - a function that prints the chessboard.
+ * @a: array
+ * Return: always 0 (success)
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 0; i++)
+{
+for (j = 0; j < 0; j++)
+	_putchar(a[i][j]);
+_putchar('\n');
+}
+}
+
