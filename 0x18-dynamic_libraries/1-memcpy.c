29d1404c2d7832629f676dc390d32be003179a94
jayson07 <jayelegant007@gmail.com>
2023-08-21 15:04:47 -0700

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: memory where it is stored
+ * @src: memory where it is copied
+ * @n: number of bytes
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int r = 0;
+int i = n;
+for (; r < i; r++)
+{
+dest[r] = src[r];
+n--;
+}
+return (dest);
+}
